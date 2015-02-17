cf1413feeb4e15ad9df928d82dab3fe1896a819c
Martin Heimbring <martin.heimbring@gmail.com>
2015-02-18 00:00:59 +0100

    implement basic CRUD functionality for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,35 @@
 class PostsController < Rulers::Controller
 
-  def show
-    @post = Post.find(params["id"])
-  end
-
+  # Create
   def new
-    @post = Post.create "title" => "Neuer Post!"
-    render_response :show, {:env => env}
+    @post = Post.create "title" => params["title"]
+    render_response :show
   end
 
+  # Read
+  def show
+    @post = find_post
+  end
+
+  # Update
+  def edit
+    @post = find_post
+    @post["title"] = "Updated!!!"
+    @post.save
+    render_response :show
+  end
+
+  # Delete
+  def destroy
+    @post = find_post
+    @post.delete!
+    render_response :destroyed
+  end
+
+  private
+
+    def find_post
+      Post.find(params["id"])
+    end
 
 end
--- mini_migration.rb
+++ mini_migration.rb
@@ -4,6 +4,16 @@ require "sqlite3"
 conn = SQLite3::Database.new "test.db"
 # run some sql in that db that will create a table called "post" with 4 columns: id, posted, title, body
 # note to self: I can have code like that generated for me using vertabelo
+
+# Basic syntax of CREATE TABLE statement is as follows:
+#  "CREATE TABLE database_name.table_name(
+#    column1 datatype PRIMARY KEY(one or more columns),
+#    column2 datatype,
+#    column3 datatype,
+#       .....
+#    columnN datatype,
+#  );
+
 conn.execute <<SQL
 create table post(
 id INTEGER PRIMARY KEY,
