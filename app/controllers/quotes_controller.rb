58e2fbc694decf281091630c383c7ef57d86b4b4
Martin Heimbring <martin.heimbring@gmail.com>
2015-02-17 11:54:37 +0100

    add index and new actions to quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,6 +5,27 @@ class QuotesController < Rulers::Controller
     render :a_quote, :noun => :winking
   end
 
+
+  def new_quote
+    attrs = {
+        "submitter" => "web user",
+        "quote" => "A picture is woth one k pixels",
+        "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
   def exception
     raise "It's bad!"
   end
